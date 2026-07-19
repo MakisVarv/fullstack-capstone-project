245c073ece7dbef1ad6247d1b080203c78ab3d91
MakisVarv <christomvarv@yahoo.com>
2026-07-19 10:47:49 -0400

    Complete Registration full  cycle

--- giftlink-frontend/src/components/RegisterPage/RegisterPage.js
+++ giftlink-frontend/src/components/RegisterPage/RegisterPage.js
@@ -1,14 +1,49 @@
 import React, { useState } from 'react';
 import './RegisterPage.css';
+import { urlConfig } from '../../config';
+import { useNavigate } from 'react-router-dom';
+import { useAppContext } from '../../context/AuthContext';
 
 function RegisterPage() {
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [showerr, setShowerr] = useState('');
+
+    const navigate = useNavigate();
+    const { setIsLoggedIn } = useAppContext();
+
 
     const handleRegister = async () => {
-        console.log('Register invoked');
+        try{
+            const response = await fetch(`${urlConfig.backendUrl}/api/auth/register`, 
+            {
+                method: "POST",	headers: {
+                    'content-type': 'application/json',
+                },body: JSON.stringify({
+                                firstName: firstName,
+                                lastName: lastName,
+                                email: email,
+                                password: password
+                            })                  
+           });    
+           const json = await response.json();
+           if (json.authtoken) {
+            sessionStorage.setItem('auth-token', json.authtoken);
+            sessionStorage.setItem('name', firstName);
+            sessionStorage.setItem('email', json.email);
+            setIsLoggedIn(true);
+            navigate('/app')
+            }
+            if (json.error) {
+                setShowerr(json.error);
+            }
+        }
+        catch (e) 
+        {
+            console.log("Error fetching details: " + e.message);
+        }
     };
 
     return (
@@ -89,6 +124,7 @@ function RegisterPage() {
                                 Login
                             </a>
                         </p>
+                        <div className="text-danger">{showerr}</div>
                     </div>
                 </div>
             </div>
